c71c35c810bbadd32de602fe040f2cc0e5e92c6e
zing321 <greeneagle692@aol.com>
2015-01-11 01:59:07 -0600

    build script and basic framework

---
+++ prob03-simple-db/simpleDB/simpleDB/SimpleDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace simpleDB
+{
+    class SimpleDB
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tacos Live!");
+            var input = Console.ReadLine();
+            Console.WriteLine("Your input is: " + input);
+        }
+    }
+}
---
+++ prob03-simple-db/simpleDB/simpleDBTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using simpleDB;
+
+namespace simpleDBTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Canary()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
