5c61b359cd0c272af1c9ea810999441f4c11780e
zing321 <greeneagle692@aol.com>
2015-01-31 22:22:50 -0600

    damn it git, get your act together

---
+++ prob03-simple-db/simpleDB/simpleDB/Commit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace simpleDB
+{
+    public class Commit : IDataCommand
+    {
+        public string Message { get; private set; }
+        public int? Operation(List<string> args)
+        {
+            Transaction.Commit();
+            return null;
+        }
+    }
+}
---
+++ prob03-simple-db/simpleDB/simpleDB/Rollback.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace simpleDB
+{
+    public class Rollback : IDataCommand
+    {
+        public string Message { get; private set; }
+        public int? Operation(List<string> args)
+        {
+            Transaction.Rollback();
+            return null;
+        }
+    }
+}
